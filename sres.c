b613032016179f152c2cbcdcbcd5954add0fa6af
baskerville <nihilhill@gmail.com>
2012-07-26 20:30:23 +0200

    tabs and trailing spaces

--- sres.c
+++ sres.c
@@ -6,11 +6,11 @@ main (void) {
     Display *dpy;
     int screen, sw, sh;
     if (!(dpy = XOpenDisplay(NULL)))
-        return -1;  
-	screen = DefaultScreen(dpy);
-	sw = DisplayWidth(dpy, screen);
-	sh = DisplayHeight(dpy, screen);
+        return -1;
+    screen = DefaultScreen(dpy);
+    sw = DisplayWidth(dpy, screen);
+    sh = DisplayHeight(dpy, screen);
     printf("%ix%i\n", sw, sh);
-	XCloseDisplay(dpy);
+    XCloseDisplay(dpy);
     return 0;
 }
